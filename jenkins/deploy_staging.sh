1a46014e18bc0e024f77eb3ef12bb9f4fa978fa6
hiepph1707 <57936136+hiepph1707@users.noreply.github.com>
2021-05-24 13:40:34 +0700

    Update deploy_staging.sh

--- jenkins/deploy_staging.sh
+++ jenkins/deploy_staging.sh
@@ -2,6 +2,6 @@ TASK_DEFINITION=$(/usr/local/bin/aws ecs describe-task-definition --task-definit
 
 NEW_TASK_DEFINITION=$(echo $TASK_DEFINITION | jq --arg ECR_REPO "${ECR_REPO}:${APP_VERSION}" --arg APP_VERSION "${APP_VERSION}" --arg APP_ENV "${APP_ENV}" '.taskDefinition |.containerDefinitions[0].image = $ECR_REPO | .containerDefinitions[0].environment[0].value = $APP_VERSION |.containerDefinitions[0].environment[1].value = $APP_ENV  | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities) | del(.registeredAt) | del(."registeredBy")')
 
-TASK_VERSION=$(/usr/local/binaws ecs register-task-definition --cli-input-json "$NEW_TASK_DEFINITION" | jq --raw-output '.taskDefinition.revision')
-ECS_UPDATE=$(/usr/local/binaws ecs update-service --force-new-deployment --cluster ${STAGING_CLUSTER}  --service ${STAGING_SERVICE} --task-definition ${STAGING_TASK}:$TASK_VERSION  | jq --raw-output '.service.serviceName')
+TASK_VERSION=$(/usr/local/bin/aws ecs register-task-definition --cli-input-json "$NEW_TASK_DEFINITION" | jq --raw-output '.taskDefinition.revision')
+ECS_UPDATE=$(/usr/local/bin/aws ecs update-service --force-new-deployment --cluster ${STAGING_CLUSTER}  --service ${STAGING_SERVICE} --task-definition ${STAGING_TASK}:$TASK_VERSION  | jq --raw-output '.service.serviceName')
 
